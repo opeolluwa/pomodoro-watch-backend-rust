84b3fcc79ec211acd26fcb1a1f911c9aa9e98960
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-01-08 03:41:27 +0100

    update: add sqlx,shuttle_shared_db,

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 use axum::{routing::get, Router};
+use dotenv::dotenv;
+use shuttle_runtime::CustomError;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::env;
+use std::time::Duration;
+use tower_http::cors::{Any, CorsLayer};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+mod pkg;
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    dotenv().ok();
 
-    Ok(router.into())
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(CustomError::new)?;
+
+    //cors
+    let _cors_layer = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_origin(Any)
+        .allow_headers(Any);
+
+    // database
+    let db_connection_str = env::var("DATABASE_URL").unwrap_or_else(|_| {
+        "postgres://opeolluwa:thunderstorm@localhost:5432/pomodoro".to_string()
+    });
+
+    // set up connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
+
+    let app_state = pkg::AppState { pool };
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }))
+    // .layer(cors_layer)
+    .with_state(app_state);
+
+    Ok(app.into())
 }
