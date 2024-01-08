ad8a27ff6c72dc71e9ecc4dfa14b05d150ad030d
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-01-08 15:04:37 +0100

    refactor

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,6 +1,32 @@
+use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
 
 #[derive(Clone)]
 pub struct AppState {
     pub pool: PgPool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SignupRequest {
+    pub full_name: String,
+    pub password: String,
+    pub email: String,
+    pub occupation: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LoginRequest {
+    email: String,
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ForgottenPasswordRequest {
+    pub email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OtpRequest {
+    pub otp: String,
 }
