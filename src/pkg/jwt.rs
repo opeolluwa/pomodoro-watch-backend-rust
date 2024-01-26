ffe7c556dfee4786d83741393e241acb142e3c6a
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-01-26 01:59:52 +0100

    update: exten jwt and mailer support

--- src/pkg/jwt.rs
+++ src/pkg/jwt.rs
@@ -1,9 +1,20 @@
 use std::time::SystemTime;
 
-use axum::extract;
+use axum::{
+    async_trait,
+    extract::FromRequestParts,
+    http::{request::Parts, StatusCode},
+    response::{IntoResponse, Response},
+    Json, RequestPartsExt,
+};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 static KEYS: Lazy<Keys> = Lazy::new(|| {
     // note that in production, you will probably want to use a random SHA-256 hash or similar
@@ -14,7 +25,7 @@ static KEYS: Lazy<Keys> = Lazy::new(|| {
 // JWT claims
 #[derive(Debug, Serialize, Deserialize)]
 pub struct JwtClaims {
-    sub: String,
+   pub sub: String,
     exp: usize,
 }
 
@@ -56,5 +67,54 @@ impl Keys {
             encoding: EncodingKey::from_secret(secret),
             decoding: DecodingKey::from_secret(secret),
         }
+    }
+}
+// error types for auth errors
+#[derive(Debug)]
+pub enum AuthError {
+    WrongCredentials,
+    MissingCredentials,
+    TokenCreation,
+    InvalidToken,
+}
+
+
+// implement IntoResponse for AuthError so we can use it as an Axum response type
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
+
+
+// implement FromRequestParts for Claims (the JWT struct)
+// FromRequestParts allows us to use Claims without consuming the request
+#[async_trait]
+impl<S> FromRequestParts<S> for JwtClaims
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        // Extract the token from the authorization header
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::InvalidToken)?;
+        // Decode the user data
+        let token_data = decode::<JwtClaims>(bearer.token(), &KEYS.decoding, &Validation::default())
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        Ok(token_data.claims)
     }
 }
--- src/pkg/mailer.rs
+++ src/pkg/mailer.rs
@@ -1,13 +1,86 @@
-// user letter to send html emails
+/// the module contains the mailer logic to send emails to users
+/// the mailer is a redis pubsub client that subscribes  to a channel form where it receives publised elesewhere un the application
 use fred::prelude::*;
-pub struct Mailer {}
+use serde::Serialize;
 
-impl Mailer {
-    pub async fn test_connection() {}
+// / email template names
+pub enum EmailTemplate {
+    Welcome,
+    VerifyEmail,
+    ResetPassword,
+    NewPassword,
+    NewVerificationToken,
+}
 
-    pub async fn send_email() -> Result<(), RedisError> {
-        let publisher_client = RedisClient::default();
-        let subscriber_client = publisher_client.clone_new();
-        todo!("implement this")
+impl EmailTemplate {
+    /// get the template name as a string
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::Welcome => "welcome",
+            Self::VerifyEmail => "verify_email",
+            Self::ResetPassword => "reset_password",
+            Self::NewPassword => "new_password",
+            Self::NewVerificationToken => "new_verification_token",
+        }
+    }
+}
+
+impl From<EmailTemplate> for String {
+    fn from(template: EmailTemplate) -> Self {
+        template.as_str().to_string()
+    }
+}
+
+impl Default for EmailTemplate {
+    fn default() -> Self {
+        Self::Welcome
+    }
+}
+
+pub struct Mailer<T> {
+    pub recipient: String,
+    template: EmailTemplate,
+    pub data: Option<T>,
+}
+
+impl<T: Serialize> Mailer<T> {
+    pub fn new(recipient: &str, template: EmailTemplate, data: Option<T>) -> Self {
+        match data {
+            Some(data) => {
+                return Self {
+                    recipient: recipient.to_string(),
+                    template,
+                    data: Some(data),
+                };
+            }
+            None => {
+                return Self {
+                    recipient: recipient.to_string(),
+                    template,
+                    data: None,
+                };
+            }
+        }
+    }
+
+    pub async fn send_email(&self) {
+        Self::test_redis_connection().await.unwrap();
+        Self::test_smtp_connection().await;
+
+    }
+
+    async fn test_smtp_connection() {
+        println!("testing smtp connection")
+    }
+
+    async fn test_redis_connection() -> Result<(), RedisError> {
+        let client = RedisClient::default();
+        let _ = client.connect();
+        client.wait_for_connect().await?;
+
+        let _ = client.publish("test_channel", "hello world").await?;
+
+        println!("connected to redis");
+        Ok(())
     }
 }
